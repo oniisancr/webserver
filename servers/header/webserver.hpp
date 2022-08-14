76d998f1fc089295b920caca939de56d6251e53f
oniisancr <oniisan@163.com>
2022-08-15 04:23:05 +0800

    IO multiplexing using select

--- servers/header/webserver.hpp
+++ servers/header/webserver.hpp
@@ -4,15 +4,20 @@
 #include "simpleweb.hpp"
 #include <unistd.h> //write read
 
+#define BUFSZ 30000
 
 namespace HDE
 {
-    class WebServer:public SimpleServer
+    class WebServer : public SimpleServer
     {
-        private:
-        char buffer[30000] = {0};
+    private:
+        char buffer[BUFSZ] = {0};
+        int server_sock;
         int new_socket;
-        sockaddr_in clnt_addr;  //用于保存客户端的信息
+        sockaddr_in clnt_addr; //用于保存客户端的信息
+        int fd_max;
+        fd_set reads, copy_reads;
+        timeval timeout;
         std::string target_url;
         void accepter();
         void handler();
--- servers/src/webserver.cpp
+++ servers/src/webserver.cpp
@@ -2,18 +2,24 @@
 
 HDE::WebServer::WebServer() : SimpleServer(AF_INET, SOCK_STREAM, 0, 80, INADDR_ANY, 10)
 {
+    FD_ZERO(&reads);
+    // 初始的最大sock值
+    fd_max = get_socket()->get_sock();
+    server_sock = fd_max;
+    // 监听欢迎套接字
+    FD_SET(fd_max, &reads);
 }
 
 void HDE::WebServer::accepter()
 {
-    // sockaddr_in address = get_socket()->get_address();
     int addrlen = sizeof(clnt_addr);
-    new_socket = accept(get_socket()->get_sock(), (sockaddr *)&clnt_addr, (socklen_t *)&addrlen);
-    read(new_socket, buffer, sizeof(buffer));
+    new_socket = accept(server_sock, (sockaddr *)&clnt_addr, (socklen_t *)&addrlen);
+    FD_SET(new_socket, &reads);            //增加监视的socket
+    fd_max = std::max(fd_max, new_socket); //更新socket监视范围
 }
 void HDE::WebServer::handler()
 {
-    std::cout << "client addr: " << inet_ntoa(clnt_addr.sin_addr) << ":" << ntohs(clnt_addr.sin_port) << std::endl;
+    std::cout << "client addr: " << inet_ntoa(clnt_addr.sin_addr) << ":" << ntohs(clnt_addr.sin_port);
     // std::cout << buffer<<std::endl;
     string url = "";
     int cnt = 0;
@@ -21,7 +27,7 @@ void HDE::WebServer::handler()
     for (int i = 0; i < sizeof(buffer); i++)
     {
         char c = buffer[i];
-        if (cnt == 1 && c != ' '&&c != '/')
+        if (cnt == 1 && c != ' ' && c != '/')
             url += c;
         if (c == ' ')
             cnt++;
@@ -30,7 +36,7 @@ void HDE::WebServer::handler()
     }
     if (url.empty())
         url = "index.html";
-    std::cout << url << std::endl;
+    std::cout <<" --> "<< url << std::endl;
     target_url = url;
 }
 void HDE::WebServer::responder()
@@ -43,12 +49,40 @@ void HDE::WebServer::responder()
 }
 void HDE::WebServer::launch()
 {
+    int select_num = 0;
     while (true)
     {
+        copy_reads = reads;
+        timeout.tv_sec = 500;
+        timeout.tv_usec = 0;
         std::cout << "=====WAITING===== " << std::endl;
-        accepter();
-        handler();
-        responder();
+        if ((select_num = select(fd_max + 1, &copy_reads, nullptr, nullptr, &timeout)) == -1)
+        {
+            std::cout << "select error...try again" << std::endl;
+            continue;
+        }
+        // time out
+        if (select_num == 0)
+            continue;
+
+        for (int i = 0; i < fd_max + 1; i++)
+        {
+            if (FD_ISSET(i, &copy_reads))
+            {
+                if (i == server_sock) //有新的连接请求
+                {
+                    accepter();
+                }
+                else
+                { //需要读取并处理用户的socket
+                    FD_CLR(i, &reads);
+                    read(i, buffer, sizeof(buffer));
+                    handler();
+                    responder();
+                    close(i);
+                }
+            }
+        }
         std::cout << "=====DONE===== " << std::endl;
     }
 }
