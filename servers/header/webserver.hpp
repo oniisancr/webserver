baed53d6bea7f481e4e0b8d3d23d8571fd44a00d
oniisancr <oniisan@163.com>
2022-08-17 16:45:34 +0800

    kqueue with nonblock socket & edge-triggered event

--- servers/header/webserver.hpp
+++ servers/header/webserver.hpp
@@ -2,9 +2,15 @@
 #define __WEBSERVER_H__
 #include "simpleserver.hpp"
 #include "simpleweb.hpp"
+#include <new>
 #include <unistd.h> //write read
+#include <fcntl.h>
+#include <sys/event.h> //kevent
+#include <sys/types.h>
+#include <string>
 
-#define BUFSZ 30000
+#define BUFSZ 2048
+#define KQUEUESZ 10
 
 namespace HDE
 {
@@ -12,12 +18,14 @@ namespace HDE
     {
     private:
         char buffer[BUFSZ] = {0};
+        std::string clnt_data;
         int server_sock;
-        int new_socket;
+        int clnt_socket;
         sockaddr_in clnt_addr; //用于保存客户端的信息
-        int fd_max;
-        fd_set reads, copy_reads;
-        timeval timeout;
+        int kq;
+        struct kevent *changelist; //每次注册事件
+        struct kevent *events;     //每次返回事件
+        timespec timeout;
         std::string target_url;
         void accepter();
         void handler();
@@ -25,6 +33,7 @@ namespace HDE
 
     public:
         WebServer();
+        ~WebServer();
         void launch();
     };
 }
--- servers/src/webserver.cpp
+++ servers/src/webserver.cpp
@@ -1,32 +1,60 @@
 #include "webserver.hpp"
 
+// reference: https://dev.to/frevib/a-tcp-server-with-kqueue-527
+
 HDE::WebServer::WebServer() : SimpleServer(AF_INET, SOCK_STREAM, 0, 80, INADDR_ANY, 10)
 {
-    FD_ZERO(&reads);
-    // 初始的最大sock值
-    fd_max = get_socket()->get_sock();
-    server_sock = fd_max;
-    // 监听欢迎套接字
-    FD_SET(fd_max, &reads);
+    // timeout.tv_sec = 0;
+    // timeout.tv_nsec = 0;
+    server_sock = get_socket()->get_sock();
+    kq = kqueue();
+    // 设置非阻塞socket read和write等不会阻塞
+    int flag = fcntl(server_sock, F_GETFL, 0);
+    fcntl(server_sock, F_SETFL, flag | O_NONBLOCK);
+    // 初始化事件列表
+    changelist = new struct kevent[KQUEUESZ];
+    events = new struct kevent[KQUEUESZ];
+    //注册server_socket监听事件  边缘触发EV_CLEAR
+    EV_SET(changelist, server_sock, EVFILT_READ, EV_ADD | EV_ENABLE | EV_CLEAR, 0, 0, 0);
+    if (kevent(kq, changelist, 1, NULL, 0, NULL) == -1)
+    {
+        perror("kevent error");
+    }
+}
+HDE::WebServer::~WebServer()
+{
+    delete[] changelist;
+    delete[] events;
 }
 
 void HDE::WebServer::accepter()
 {
     int addrlen = sizeof(clnt_addr);
-    new_socket = accept(server_sock, (sockaddr *)&clnt_addr, (socklen_t *)&addrlen);
-    FD_SET(new_socket, &reads);            //增加监视的socket
-    fd_max = std::max(fd_max, new_socket); //更新socket监视范围
+    clnt_socket = accept(server_sock, (sockaddr *)&clnt_addr, (socklen_t *)&addrlen);
+    if (clnt_socket == -1)
+    {
+        perror("Accept socket error");
+        return;
+    }
+    // 设置非阻塞socket
+    int flag = fcntl(clnt_socket, F_GETFL, 0);
+    fcntl(clnt_socket, F_SETFL, flag | O_NONBLOCK);
+    //注册该socket  每次注册1个socket
+    EV_SET(changelist, clnt_socket, EVFILT_READ, EV_ADD | EV_ENABLE | EV_CLEAR, 0, 0, 0);
+    if (kevent(kq, changelist, 1, NULL, 0, NULL) == -1)
+    {
+        perror("kevent add error");
+    }
 }
 void HDE::WebServer::handler()
 {
     std::cout << "client addr: " << inet_ntoa(clnt_addr.sin_addr) << ":" << ntohs(clnt_addr.sin_port);
-    // std::cout << buffer<<std::endl;
     string url = "";
     int cnt = 0;
     // 取出需要的页面 在第一个空格到第二个空格之间 仅实现GET请求
-    for (int i = 0; i < sizeof(buffer); i++)
+    for (int i = 0; i < clnt_data.size(); i++)
     {
-        char c = buffer[i];
+        char c = clnt_data[i];
         if (cnt == 1 && c != ' ' && c != '/')
             url += c;
         if (c == ' ')
@@ -36,51 +64,61 @@ void HDE::WebServer::handler()
     }
     if (url.empty())
         url = "index.html";
-    std::cout <<" --> "<< url << std::endl;
+    std::cout << " --> " << url << std::endl;
     target_url = url;
 }
 void HDE::WebServer::responder()
 {
-    // char *hello = "hello form server";
-    // std::string res = "hello form server!";
     SimpleWeb web(target_url);
-    write(new_socket, string(web).c_str(), string(web).size());
-    close(new_socket);
+    write(clnt_socket, string(web).c_str(), string(web).size());
 }
 void HDE::WebServer::launch()
 {
-    int select_num = 0;
+    int new_events = 0;
     while (true)
     {
-        copy_reads = reads;
-        timeout.tv_sec = 500;
-        timeout.tv_usec = 0;
         std::cout << "=====WAITING===== " << std::endl;
-        if ((select_num = select(fd_max + 1, &copy_reads, nullptr, nullptr, &timeout)) == -1)
+        if ((new_events = kevent(kq, nullptr, 0, events, KQUEUESZ, nullptr)) == -1)
         {
-            std::cout << "select error...try again" << std::endl;
+            std::cout << "kevevt error...try again" << std::endl;
             continue;
         }
         // time out
-        if (select_num == 0)
+        if (new_events == 0)
             continue;
 
-        for (int i = 0; i < fd_max + 1; i++)
+        for (int i = 0; i < new_events; i++)
         {
-            if (FD_ISSET(i, &copy_reads))
+            int event_fd = events[i].ident;
+            // 断开链接 客户端发送EOF后，kqueue会自动删除相关事件
+            if (events[i].flags & EV_EOF)
             {
-                if (i == server_sock) //有新的连接请求
+                close(event_fd);
+                continue;
+            }
+            if (event_fd == server_sock) //有新的连接请求
+            {
+                accepter();
+            }
+            else if (events[i].filter & EVFILT_READ)
+            { //需要读取并处理用户的socket
+                clnt_data.clear();
+                clnt_socket = event_fd;
+                // 由于是边缘触发EV_CLEAR，所以需要一次读完缓冲
+                int str_len = 0;
+                while (true)
                 {
-                    accepter();
+                    // 0:EOF -1:error
+                    str_len = read(clnt_socket, buffer, BUFSZ - 1);
+                    // empty buffer
+                    if (str_len == -1 && errno == EAGAIN)
+                        break;
+                    buffer[str_len] = '\0';
+                    clnt_data += std::string(buffer);
                 }
-                else
-                { //需要读取并处理用户的socket
-                    FD_CLR(i, &reads);
-                    read(i, buffer, sizeof(buffer));
-                    handler();
-                    responder();
-                    close(i);
-                }
+                handler();
+                responder();
+                close(clnt_socket);
             }
         }
         std::cout << "=====DONE===== " << std::endl;
